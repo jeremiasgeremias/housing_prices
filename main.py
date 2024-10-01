6f35bfaba4b1d2b58363e9c701dcece8bb3cf0d0
jeremiasgeremias <drumsris@gmail.com>
2024-09-30 22:00:58 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,104 @@
+# %%vitual
+import os
+import tarfile
+from six.moves import urllib
+import numpy as np
+import pandas as pd
+import hashlib
+
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
+HOUSING_PATH = os.path.join("datasets", "housing")
+HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+
+# %%
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+    if not os.path.isdir(housing_path):
+        os.makedirs(housing_path)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+
+fetch_housing_data()
+
+
+# %%
+def load_housing_data(housing_path=HOUSING_PATH):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
+# %%
+## look at the data
+housing = load_housing_data()
+housing.head()
+# %%
+housing.info()
+# %%
+housing["ocean_proximity"].value_counts(dropna=False)
+# %%
+def split_train_test(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+
+train_set, test_set = split_train_test(housing, 0.2)
+print(len(train_set), "train +", len(test_set), "test")
+# %%
+def test_set_check(identifier, test_ratio, hash):
+     return hash(np.int64(identifier)).digest()[-1] < 256 * test_ratio
+
+def split_train_test_by_id(data, test_ratio, id_column, hash=hashlib.md5):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio, hash))
+    return data.loc[~in_test_set], data.loc[in_test_set]
+
+housing_with_id = housing.reset_index()
+train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "index")
+# %%
+from sklearn.model_selection import train_test_split
+train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+# %%
+# Now we suppose we actully want to have a some guaranteed characteristic of
+# the test and train set to be in the same proportion
+# All that is to avoid some chance of choosing a sample of our dataset
+# that is not representative of the whole dataset
+
+housing["income_cat"] = np.ceil(housing["median_income"] / 1.5)
+housing["income_cat"].where(housing["income_cat"] < 5, 5.0, inplace=True)
+
+from sklearn.model_selection import StratifiedShuffleSplit
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+for train_index, test_index in split.split(housing, housing["income_cat"]):
+    strat_train_set = housing.loc[train_index]
+    strat_test_set = housing.loc[test_index]
+
+strat_test_set["income_cat"].value_counts() / len(strat_test_set)
+
+# %%
+for set_ in (strat_train_set, strat_test_set):
+    set_.drop("income_cat", axis=1, inplace=True)
+# %%
+housing = strat_train_set.copy()
+# %%
+%matplotlib inline
+housing.plot(kind="scatter", x="longitude", y="latitude", alpha = 0.1)
+# %%
+import matplotlib.pyplot as plt
+housing.plot(kind = "scatter", x="longitude", y="latitude", alpha=0.4,
+            s=housing["population"]/100, label="population", figsize=(10,7),
+            c="median_house_value", cmap = plt.get_cmap("jet"), colorbar=True,)
+# %%
+corr_matrix = housing.corr(numeric_only=True)
+corr_matrix["median_house_value"].sort_values(ascending=False)
+# %%
+from pandas.plotting import scatter_matrix
+attributes = ["median_house_value", "median_income", 
+              "total_rooms", "housing_median_age"]
+scatter_matrix(housing[attributes], figsize=(12,8))
+# %%
+housing.plot(kind="scatter", x="median_income", y="median_house_value", alpha=0.1)
+# %%
